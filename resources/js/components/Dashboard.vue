36374ce448c56cde33bcfadce43285a406c3fc07
fredochieng <34058095+fredochieng@users.noreply.github.com>
2018-12-15 20:03:49 +0300

    second commit

---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Dashboard Component</div>
+
+                    <div class="card-body">
+                        I'm an example of teh dashboard component.my counterpart profile is still down, i repeat,
+                        my counterpart profile is still down and it's not working
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
